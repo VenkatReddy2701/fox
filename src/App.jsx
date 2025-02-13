bdb54b794e13d4086caf740d88bb2c9e468e9c85
VenkatReddy2701 <pulimireddy2701@gmail.com>
2025-02-13 17:35:50 +0530

    made-changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>
-        Hello I am react developer</h1>
+      <h1>Hello I am react developer</h1>
     </div>
   )
 }
